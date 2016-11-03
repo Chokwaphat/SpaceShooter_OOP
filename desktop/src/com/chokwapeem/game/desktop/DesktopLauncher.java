078f94771f360c1906eb517d8362ed17df241692
Chokwaphat <chokwaphat@Chokwaphats-MacBook-Pro.local>
2016-11-03 22:45:49 +0700

    First commit

---
+++ desktop/src/com/chokwapeem/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.chokwapeem.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.chokwapeem.game.Spaceshooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Spaceshooter(), config);
+	}
+}

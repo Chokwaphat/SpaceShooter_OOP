76d74a075dd125f7ee325e2ff74af4046d50327f
Chokwaphat <chokwaphat@Chokwaphats-MacBook-Pro.local>
2016-11-04 00:09:30 +0700

    WithKeyBoardInput

--- core/src/com/chokwapeem/game/Spaceshooter.java
+++ core/src/com/chokwapeem/game/Spaceshooter.java
@@ -2,10 +2,13 @@ package com.chokwapeem.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.chokwapeem.managers.GameInputProcessor;
+import com.chokwapeem.managers.GameKeys;
 
 public class Spaceshooter extends ApplicationAdapter {
 	
@@ -22,12 +25,18 @@ public class Spaceshooter extends ApplicationAdapter {
 		
 		cam = new OrthographicCamera(WIDTH, HEIGHT);
 		cam.translate(WIDTH / 2, HEIGHT / 2);
+		cam.update();
+		
+		Gdx.input.setInputProcessor(new GameInputProcessor());
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		GameKeys.update();
+		
 		
 	}
 	

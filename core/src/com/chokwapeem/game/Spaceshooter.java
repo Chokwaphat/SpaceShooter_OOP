43d53481cdf9e9fb7576c98d9aada80c042b922b
Chokwaphat <chokwaphat@Chokwaphats-MacBook-Pro.local>
2016-11-03 23:11:59 +0700

    SetUpTheCamera

--- core/src/com/chokwapeem/game/Spaceshooter.java
+++ core/src/com/chokwapeem/game/Spaceshooter.java
@@ -3,31 +3,36 @@ package com.chokwapeem.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Spaceshooter extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	
+	public static int WIDTH;
+	public static int HEIGHT;
+	
+	public static OrthographicCamera cam;
+	
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		WIDTH = Gdx.graphics.getWidth();
+		HEIGHT = Gdx.graphics.getHeight();
+		
+		cam = new OrthographicCamera(WIDTH, HEIGHT);
+		cam.translate(WIDTH / 2, HEIGHT / 2);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		
 	}
 }
--- desktop/src/com/chokwapeem/game/desktop/DesktopLauncher.java
+++ desktop/src/com/chokwapeem/game/desktop/DesktopLauncher.java
@@ -7,6 +7,14 @@ import com.chokwapeem.game.Spaceshooter;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Spaceshooter";
+		config.width = 600;
+		config.height = 400;
+		config.useGL30 = false;
+		config.resizable = false;
+		
 		new LwjglApplication(new Spaceshooter(), config);
+		
+		
 	}
 }

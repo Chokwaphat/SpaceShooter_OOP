f3e4f328cd164341b28294ee8b5cb6e7c512d4c9
Chokwaphat <chokwaphat@Chokwaphats-MacBook-Pro.local>
2016-11-07 23:16:07 +0700

    CanShoot

--- core/src/com/chokwapeem/game/PlayState.java
+++ core/src/com/chokwapeem/game/PlayState.java
@@ -1,53 +1,88 @@
 package com.chokwapeem.game;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class PlayState extends GameState {
-	
+
 	private ShapeRenderer sr;
-	
+
 	private Player player;
 	private Player2 player2;
 	
+	private ArrayList<Bullet> bullets;
+
 	public PlayState(GameStateManager gsm) {
-		super(gsm);
 		
+		super(gsm);
+
 	}
 
 	@Override
 	public void init() {
+		
 		sr = new ShapeRenderer();
-		player = new Player();
-		player2 = new Player2();
+		
+		bullets = new ArrayList<Bullet>();
+		player = new Player(bullets);
+		player2 = new Player2(bullets);
 	}
 
 	@Override
 	public void update(float dt) {
+		
 		handleInput();
+
 		player.update(dt);
+
 		player2.update(dt);
+		
+		for(int i = 0; i < bullets.size(); i++) {
+			
+			bullets.get(i).update(dt);
+			if(bullets.get(i).shouldRemove()) {
+				bullets.remove(i);
+				i--;
+			}
+		}
 	}
 
 	@Override
 	public void draw() {
 		player.draw(sr);
+
 		player2.draw(sr);
+		
+		for(int i = 0; i < bullets.size(); i++) {
+			bullets.get(i).draw(sr);
+		}
 	}
 
 	@Override
 	public void handleInput() {
+		
 		player.setLeft(GameKeys.isDown(GameKeys.LEFT));
 		player.setRight(GameKeys.isDown(GameKeys.RIGHT));
 		player.setUp(GameKeys.isDown(GameKeys.UP));
+		if(GameKeys.isPressed(GameKeys.SHIFT)) {
+			
+			player.shoot();
+		}
+
 		player2.setLeft(GameKeys.isDown(GameKeys.LEFT2));
 		player2.setRight(GameKeys.isDown(GameKeys.RIGHT2));
 		player2.setUp(GameKeys.isDown(GameKeys.UP2));
+		if(GameKeys.isPressed(GameKeys.SPACE)) {
+			
+			player2.shoot();
+		}
 	}
 
 	@Override
 	public void dispose() {
-		
+
 	}
-	
-	
+
+
 }
--- core/src/com/chokwapeem/game/Player.java
+++ core/src/com/chokwapeem/game/Player.java
@@ -1,5 +1,7 @@
 package com.chokwapeem.game;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
@@ -8,98 +10,119 @@ import com.chokwapeem.game.Spaceshooter;
 
 public class Player extends SpaceObject {
 	
+	private ArrayList<Bullet> bullets;
+	private final int MAX_BULLETS = 7;
+	
 	private boolean left;
 	private boolean right;
 	private boolean up;
-	
+
 	private float maxSpeed;
 	private float accelerate;
 	private float decelerate;
-	
-	public Player () {
+
+	public Player (ArrayList<Bullet> bullets) {
 		
+		this.bullets = bullets;
 		x = Spaceshooter.WIDTH / 2 - 200;
 		y = Spaceshooter.HEIGHT / 2;
-		
+
 		maxSpeed = 300;
 		accelerate = 200;
 		decelerate = 10;
-		
+
 		shapex = new float[4];
 		shapey = new float[4];
-
+		
 		radians = (float)Math.PI  / 2;
 		rotationSpeed = 3;
-		
-		
+
 	}
 	
 	private void setShape() {
 		shapex[0] = x + MathUtils.cos(radians) * 13;
 		shapey[0] = y + MathUtils.sin(radians) * 13;
+
 		shapex[1] = x + MathUtils.cos(radians - 4 * (float)Math.PI / 5) * 13;
 		shapey[1] = y + MathUtils.sin(radians - 4 * (float)Math.PI  / 5) * 13;
+
 		shapex[2] = x + MathUtils.cos(radians + (float)Math.PI ) * 5;
 		shapey[2] = y + MathUtils.sin(radians + (float)Math.PI ) * 5;
+
 		shapex[3] = x + MathUtils.cos(radians + 4 * (float)Math.PI  / 5) * 13;
 		shapey[3] = y + MathUtils.sin(radians + 4 * (float)Math.PI  / 5) * 13;
-		
+
 	}
-	
+
 	public void setLeft(boolean b) {
+		
 		left = b;
 	}
-	
+
 	public void setRight(boolean b) {
+		
 		right = b;
 	}
-	
+
 	public void setUp(boolean b) {
+		
 		up = b;
 	}
 	
+	public void shoot() {
+		
+		if(bullets.size() == MAX_BULLETS) {
+			
+			return;
+		}
+		
+		bullets.add(new Bullet(x, y, radians));
+	}
+	
 	public void update(float dt) {
+		
 		if(left) {
-			radians += rotationSpeed * dt;	
+			radians += rotationSpeed * dt;
 		} else if(right) {
 			radians -= rotationSpeed * dt;
 		}
-		
+
 		if(up) {
 			dx += MathUtils.cos(radians) * accelerate * dt;
 			dy += MathUtils.sin(radians) * accelerate * dt;
+			
 		}
-		
+
 		float vec = (float) Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 		if(vec > 0) {
 			dx -= (dx / vec) * decelerate * dt;
 			dy -= (dy / vec) * decelerate * dt;
 		}
-		
+
 		if(vec > maxSpeed) {
 			dx = (dx / vec) * maxSpeed;
 			dy = (dy / vec) * maxSpeed;
 		}
-		
+
 		x += dx * dt;
 		y += dy * dt;
-		
+
 		setShape();
+
 		wrap();
 	}
-	
+
 	public void draw(ShapeRenderer sr) {
-		
-		
+
 		sr.setColor(255, 255, 0, 1);
-		
+
 		sr.begin(ShapeType.Line);
-		
+
 		for(int i = 0, j = shapex.length - 1; i < shapex.length; j = i++) {
 			sr.line(shapex[i], shapey[i], shapex[j], shapey[j]);
 		}
 		
 		sr.end();
-		
-	}	
+
+	}
 }
